7c8e5aaa83fbc4523785774310ad27522e50c242
Dawid Harendarz <dharendarz2@gmail.com>
2023-07-17 20:33:49 +0200

    feat: change styles and js

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,48 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import './styles/global.scss';
+
+const submitFormFilterRoles = () => {
+
+    const selectValue = document.querySelector('#js-user-roles');
+
+    if (selectValue) {
+        selectValue.addEventListener('change', () => {
+            document.querySelector('#js-user-filter').submit()
+        })
+    }
+
+}
+
+const submitFormFilterPagination = () => {
+    const selectValue = document.querySelector('#js-user-pagination');
+
+    if (selectValue) {
+        selectValue.addEventListener('change', () => {
+            document.querySelector('#js-user-filter').submit()
+        })
+    }
+
+}
+
+const submitSelectFilters = () => {
+    const formFilter = document.querySelector('#js-filter');
+
+    if (formFilter) {
+        const selects = formFilter.querySelectorAll('select') ?? [];
+        selects.forEach(select => {
+            select.addEventListener('change', () => {
+                formFilter.submit();
+            })
+        })
+
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    submitFormFilterRoles();
+    submitFormFilterPagination();
+    submitSelectFilters();
+})
